5b18f3e255bd24a3e66c3efb358d879538b6deb0
sjpratt19 <sjpratt19@gmail.com>
2020-08-24 17:51:49 -0700

    Updated site data

---
+++ src/pages/resume.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Resume = () => (
+  <Layout>
+    <SEO title="Resume" />
+    <h1>Resume</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+)
+
+export default Resumeesume
