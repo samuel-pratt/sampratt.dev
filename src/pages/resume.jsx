c2fb355f6e15b0b6a6222ead45dba9e5491d5ceb
sjpratt19 <sjpratt19@gmail.com>
2020-09-05 09:44:10 -0700

    Fixing css error

---
+++ src/pages/resume.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Resume</h1>
+    <p>Hello kiana</p>
+  </Layout>
+)
+
+export default IndexPage
